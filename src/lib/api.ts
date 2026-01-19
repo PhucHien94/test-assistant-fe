a7799bc272c849c7b23195028de495e923ee2fc2
Phuc Hien <phuchien94@gmail.com>
2026-01-19 09:57:06 +0700

    Change BE URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useAuthStore } from "../state/auth";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000",
+  baseURL: import.meta.env.VITE_PROD_BE_URL || 'http://localhost:3000',
 });
 
 // Before send request
